4ffab6132811300835872b37d8ed6bd53b6c4eb8
khalid09dev <mdkhalidhossen@hotmail.com>
2024-12-16 17:40:09 +0600

    initialized project and added homepage, dashboard component and some invoice and customer route

---
+++ app/ui/fonts.ts
@@ -0,0 +1,4 @@
+import { Inter, Lunasima } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+export const lunasima = Lunasima({ subsets: ['vietnamese'], weight: "700" });

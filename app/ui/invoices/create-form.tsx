b593b63ca61802224e150d6e6601728ca37c536e
khalid09dev <mdkhalidhossen@hotmail.com>
2024-12-18 12:40:03 +0600

    fixed linting issues in create form, buttons, and invoice edit page

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { CustomerField, InvoiceForm } from '@/app/lib/definitions';
+import { CustomerField } from '@/app/lib/definitions';
 import Link from 'next/link';
 import {
   CheckIcon,
@@ -12,7 +12,7 @@ import { Button } from '@/app/ui/button';
 import { createInvoice, State } from '@/app/lib/actions';
 import { useActionState } from 'react';
 
-export default function Form({ customers, invoice }: { customers: CustomerField[], invoice: InvoiceForm }) {
+export default function Form({ customers }: { customers: CustomerField[] }) {
   const initialState: State = { message: null, errors: {} }; 
   const [state, formAction] = useActionState(createInvoice, initialState);
 
@@ -29,7 +29,7 @@ export default function Form({ customers, invoice }: { customers: CustomerField[
               id="customer"
               name="customerId"
               className="peer block w-full cursor-pointer rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
-              defaultValue={invoice.customer_id || ""}
+              defaultValue=""
               aria-describedby='customer-error'
             >
               <option value="" disabled>
@@ -68,7 +68,6 @@ export default function Form({ customers, invoice }: { customers: CustomerField[
                 placeholder="Enter USD amount"
                 className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
                 required
-                defaultValue={invoice.amount || ""}
               />
               <CurrencyDollarIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
             </div>
@@ -90,7 +89,6 @@ export default function Form({ customers, invoice }: { customers: CustomerField[
                   value="pending"
                   className="h-4 w-4 cursor-pointer border-gray-300 bg-gray-100 text-gray-600 focus:ring-2"
                   required
-                  defaultChecked={invoice.status === 'pending'}
                 />
                 <label
                   htmlFor="pending"
@@ -107,7 +105,6 @@ export default function Form({ customers, invoice }: { customers: CustomerField[
                   value="paid"
                   className="h-4 w-4 cursor-pointer border-gray-300 bg-gray-100 text-gray-600 focus:ring-2"
                   required
-                  defaultChecked={invoice.status === 'paid'}
                 />
                 <label
                   htmlFor="paid"
